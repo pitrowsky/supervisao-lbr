1c9dd23c9202a1fc22b764e645f93172583fd191
Gustavo Pitrowsky <gustavopitrowsky@gmail.com>
2025-07-21 21:31:27 -0300

    Resolvendo bugs front-end

--- login.js
+++ login.js
@@ -1,33 +1,35 @@
-function passwordView() {
-    const password = document.getElementById('password');
-    const iconVis = document.querySelector('.fa-solid.fa-eye');
-    const iconInv = document.querySelector('.fa-solid.fa-eye-slash');
+document.addEventListener('DOMContentLoaded', () => {
+    function passwordView() {
+        const password = document.getElementById('password');
+        const iconVis = document.querySelector('.fa-solid.fa-eye');
+        const iconInv = document.querySelector('.fa-solid.fa-eye-slash');
 
-    if (iconInv.classList.contains('active')) {
-        iconInv.classList.remove('active');
-        iconVis.classList.add('active');
-        password.type = 'text';
+        if (iconInv.classList.contains('active')) {
+            iconInv.classList.remove('active');
+            iconVis.classList.add('active');
+            password.type = 'text';
+        }
+        else if (iconVis.classList.contains('active')) {
+            iconVis.classList.remove('active');
+            iconInv.classList.add('active');
+            password.type = 'password';
+        }
     }
-    else if (iconVis.classList.contains('active')) {
-        iconVis.classList.remove('active');
-        iconInv.classList.add('active');
-        password.type = 'password';
-    }
-}
 
-document.getElementById('login-form').addEventListener('submit', async (event) => {
-    event.preventDefault(); // Impede o envio padrão do formulário
+    document.getElementById('icon-password').onclick = passwordView;
 
-    // Captura os valores dos campos de usuário e senha
-    const username = document.getElementById('user').value;
-    const password = document.getElementById('password').value;
+    document.getElementById('login-form').addEventListener('submit', async (event) => {
+        event.preventDefault();
 
-    // Envia uma requisição POST para o backend com os dados do login
-    await fetch('https://backend-lbr.vercel.app/users', {
-        method: 'POST',
-        headers: {
-            'Content-Type': 'application/json',
-        },
-        body: JSON.stringify({ username: username, password: password })
+        const username = document.getElementById('user').value;
+        const password = document.getElementById('password').value;
+
+        await fetch('https://backend-lbr.vercel.app/users', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify({ username, password })
+        });
     });
 });
